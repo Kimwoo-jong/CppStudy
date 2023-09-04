62a9d3d41f6d9e86f62739d0aaae8237fc870692
Kimwoojong <wooj950713@naver.com>
2023-09-04 18:26:38 +0900

    2023/09/04 18:26
    Study List

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -3,26 +3,33 @@ using namespace std;
 #include <vector>
 #include <list>
 #include <queue>
-#include "Vector.h"
+#include "List.h"
 
 int main()
 {
-	vector<int> v;
-	v.push_back(10);
-	v.push_back(20);
-	v.push_back(30);
+	// size (resize)
+	// capacity (reserve)
+	// 삽입/삭제
+	// - 시작		O(1)
+	// - 중간		O(1)
+	// - 끝			O(1)
+	// front		O(1)
+	// back			O(1)
+	// push_front	O(1)
+	// push_back	O(1)
+	// 임의 접근 li[2]  X
 
-	vector<int>::iterator it;
+	List<int> li;
 
-	for (it = v.begin(); it != v.end(); it++)
+	li.AddAtTail(10);
+	li.AddAtTail(20);
+	li.AddAtTail(30);
+
+	for (List<int>::iterator it = li.begin(); it != li.end(); it++)
 	{
-		int data = *it;
-		cout << data << endl;
-	}
+		int value = *it;
 
-	/*it++;
-	v.begin();
-	v.end();
-	it != v.end();
-	*it;*/
+		if (value == 3)
+			break;
+	}
 }
