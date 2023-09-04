7fd7600b749c941b714af20a8ab1dcd7c0a04594
Kimwoojong <wooj950713@naver.com>
2023-09-04 18:05:21 +0900

    2023/09/04 18:05
    Study iterator

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -1,61 +1,28 @@
 ﻿#include <iostream>
 using namespace std;
 #include <vector>
+#include <list>
 #include <queue>
+#include "Vector.h"
 
 int main()
 {
-	// size(resize)
-	// capacity(reserve)
-	// 삽입/삭제
-	// - 시작	O(N)
-	// - 중간	O(N)
-	// - 끝		O(1)
-	// push_back, front, back
-	// 임의 접근 v[i]
-	// v.remove(10)?
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
 
-	// 추가
-	// 삭제
-	// 순회
-	// 검색
+	vector<int>::iterator it;
 
-	//vector<int> v(5);
-	//vector<int> v(5, -1);
-	//vector<int> v{1, 2, 3, 4, 5};
-
-	//v.clear();
-
-	// size		- 줄어든다.
-	// capacity - 줄어들지 않음.
-
-	/*cout << v.size() << endl;
-	cout << v.capacity() << endl;*/
-
+	for (it = v.begin(); it != v.end(); it++)
 	{
-		// iterator ? 반복자 포인터?
-		vector<int> v{1, 2, 3, 4, 5};
-
-		vector<int>::iterator it;
-
-		for (it = v.begin(); it != v.end(); it++)
-		{
-			int data = *it;
-			if (data == 3)
-			{
-				// 찾음
-				break;
-			}
-		}
-
-		if (it != v.end())
-		{
-			cout << "찾음" << endl;
-			v.erase(it);
-		}
-		else
-		{
-			cout << "찾지 못함" << endl;
-		}
+		int data = *it;
+		cout << data << endl;
 	}
+
+	/*it++;
+	v.begin();
+	v.end();
+	it != v.end();
+	*it;*/
 }
