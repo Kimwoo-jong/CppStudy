7afc611cb4a12a8db5f516069626311e8bb58c14
Kimwoojong <wooj950713@naver.com>
2023-09-05 20:33:07 +0900

    2023/09/05 20:32
    Study R_value Reference

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -7,45 +7,107 @@
 #include <algorithm>
 using namespace std;
 
+class Pet
+{
 
+};
+
+class Knight
+{
+public:
+	Knight()
+	{
+
+	}
+
+	~Knight()
+	{
+		if (_pet)
+			delete _pet;
+	}
+
+	// 복사 생성자
+	Knight(const Knight& knight)
+	{
+
+	}
+
+	// 복사 대입 연산자
+	void operator=(const Knight& knight)
+	{
+		_hp = knight._hp;
+
+		// 둘 다 동일한 원본 펫을 가리키고 있으므로,
+		// 소멸의 순간 두 개가 삭제되는데 실질적인 Pet은 한 개이기 때문 (얕은 복사)
+		//_pet = knight._pet;
+		if (knight._pet)
+			_pet = new Pet(*knight._pet);
+	}
+
+	// 이동 생성자
+	Knight(Knight&& knight) noexcept
+	{
+		_hp = knight._hp;
+		_pet = knight._pet;
+		knight._pet = nullptr;
+	}
+
+	// 이동 대입 연산자
+	// pet의 소유권을 이전했다
+	void operator=(Knight&& knight) noexcept
+	{
+		_hp = knight._hp;
+		_pet = knight._pet;
+		knight._pet = nullptr;
+	}
+public:
+	int _hp;
+	Pet* _pet = nullptr;
+};
+
+void TestKnightCopy(Knight knight)
+{
+	knight._hp = 100;
+}
+
+// 원본을 넘겨줄테니 건드려도 됨.
+void TestKnight_LValueRef(Knight& knight)
+{
+	knight._hp = 100;
+}
+
+// 원본은 넘겨주되 건드릴 수는 없다.
+void TestKnight_ConstLValueRef(const Knight& knight)
+{
+	//knight._hp = 100;
+}
+
+// 오른값 참조 (&&)
+// 원본을 넘겨준다 -> 더 이상 활용하지 않을테니 마음대로 해(일회성)
+void TestKnight_RValueRef(Knight&& knight)
+{
+	//knight._hp = 100;
+}
 
 int main()
 {
-	// 1) ASCII 코드 'a' = 97, 'A' = 65
-	char ch = 'a';
-	// 7bit (0 ~ 127)
+	// C++11 오른값 참조
+	// 왼값(l-value) vs 오른값(r-value)
 
-	// 2) ANSI >> 동일한 키코드 값 = 동일한 문자
-	// // ASCII + 각 국 언어별로 128
-	// CP949(한국어)
+	// l-value : 단일식을 넘어서 계속 지속되는 개체
+	// r-value : l-value가 아닌 나머지
+	int a = 3;
 
-	// 3) 유니코드 (동일 번호 = 동일한 문자 = 동일한 유니코드)
-	// 인코딩(UTF-8)
-	// - UTF-8	: 영어(1), 한국어/중국어(3)
-	// - UTF-16 : 영어(2), 한국어(2), 중국어(2)
+	Knight k1;
+	k1._pet = new Pet();
 
-	// 4) MBCS(Multi Bytes Character Set) vs WBCS(Wide Byte Character Set)
-	// - 멀티바이트 집합	: 가변 길이 인코딩	>> UTF-8
-	// - 유니코드 집합	: 고정 길이 인코딩	>> UTF-16
+	Knight k2;
+	//k2 = static_cast<Knight&&>(k1);
+	k2 = std::move(k1); // << rValue_cast
 
-	setlocale(LC_ALL, "");
-	cout << "LC_ALL : " << setlocale(LC_ALL, NULL) << endl;
-
-	// CP949(한국어)
-	const char* test = "안녕하세요";
-	cout << test << endl;
-
-	//
-	auto test2 = u8"aaa김우종입니당";
-	setlocale(LC_ALL, "en_US.UTF-8");
-	cout << test2 << endl;
-
-	//
-	auto test3 = u"김우종데스";
-
-	// char // string
-	wchar_t ch1 = L'김';
-	wstring name = L"김우종데스웅";
-
-	wcout << name << endl;
+	//TestKnight_LValueRef(k1);
+	//TestKnight_LValueRef(Knight()); << 안됨 (const를 붙여야 함)
+	//TestKnight_ConstLValueRef(Knight());
+	//TestKnight_RValueRef(Knight());
+	//TestKnight_RValueRef(static_cast<Knight&&>(k1));
 }
