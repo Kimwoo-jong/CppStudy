b6a5f3e857496c351bb4a6a4c473e61ed581d04a
Kimwoojong <wooj950713@naver.com>
2023-09-05 20:57:03 +0900

    2023/09/05 20:56
    Study Shared_ptr

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -7,107 +7,123 @@
 #include <algorithm>
 using namespace std;
 
-class Pet
-{
-
-};
-
 class Knight
 {
 public:
-	Knight()
+	~Knight() { }
+	void Attack()
 	{
-
+		if (_target)
+			_target->_hp -= _damage;
 	}
 
-	~Knight()
-	{
-		if (_pet)
-			delete _pet;
-	}
-
-	// 복사 생성자
-	Knight(const Knight& knight)
-	{
-
-	}
-
-	// 복사 대입 연산자
-	void operator=(const Knight& knight)
-	{
-		_hp = knight._hp;
-
-		// 둘 다 동일한 원본 펫을 가리키고 있으므로,
-		// 소멸의 순간 두 개가 삭제되는데 실질적인 Pet은 한 개이기 때문 (얕은 복사)
-		//_pet = knight._pet;
-		if (knight._pet)
-			_pet = new Pet(*knight._pet);
-	}
-
-	// 이동 생성자
-	Knight(Knight&& knight) noexcept
-	{
-		_hp = knight._hp;
-		_pet = knight._pet;
-		knight._pet = nullptr;
-	}
-
-	// 이동 대입 연산자
-	// pet의 소유권을 이전했다
-	void operator=(Knight&& knight) noexcept
-	{
-		_hp = knight._hp;
-		_pet = knight._pet;
-		knight._pet = nullptr;
-	}
 public:
-	int _hp;
-	Pet* _pet = nullptr;
+	int _hp = 100;
+	int _damage = 10;
+	shared_ptr<Knight> _target = nullptr;
 };
 
-void TestKnightCopy(Knight knight)
+class RefCountBlock
 {
-	knight._hp = 100;
-}
+public:
+	int _refCount = 1;
+};
 
-// 원본을 넘겨줄테니 건드려도 됨.
-void TestKnight_LValueRef(Knight& knight)
+template<typename T>
+class SharedPtr
 {
-	knight._hp = 100;
-}
+public:
+	SharedPtr() { }
+	SharedPtr(T* ptr) : _ptr(ptr)
+	{
+		if (ptr)
+		{
+			_block = new RefCountBlock();
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
 
-// 원본은 넘겨주되 건드릴 수는 없다.
-void TestKnight_ConstLValueRef(const Knight& knight)
-{
-	//knight._hp = 100;
-}
+	SharedPtr(const SharedPtr& other) : _ptr(other._ptr), _block(other._block)
+	{
+		if (_ptr)
+		{
+			_block->_refCount++;
+		}
+	}
 
-// 오른값 참조 (&&)
-// 원본을 넘겨준다 -> 더 이상 활용하지 않을테니 마음대로 해(일회성)
-void TestKnight_RValueRef(Knight&& knight)
+	~SharedPtr()
+	{
+		if (_ptr)
+		{
+			_block->_refCount--;
+
+			if (_block->_refCount == 0)
+			{
+				delete _ptr;
+				delete _block;
+				cout << "Delete Data" << endl;
+			}
+		}
+	}
+
+	void operator=(const SharedPtr& other)
+	{
+		_ptr = other._ptr;
+		_block = other._block;
+
+		if (_ptr)
+			_block->_refCount++;
+	}
+
+public:
+	T* _ptr;
+	RefCountBlock* _block;
+
+};
+
+void Test(shared_ptr<Knight>& k)
 {
-	//knight._hp = 100;
+	// 여기서 refCount는 +1됨
 }
 
 int main()
 {
-	// C++11 오른값 참조
-	// 왼값(l-value) vs 오른값(r-value)
+	// 스마트 포인터
+	// shared_ptr	: 
+	// weak_ptr		: 
+	// unique_ptr	: 
 
-	// l-value : 단일식을 넘어서 계속 지속되는 개체
-	// r-value : l-value가 아닌 나머지
-	int a = 3;
+	{
+		// 이 방식에서는 몇 명이 가리키는지 모름.
+		Knight* k1 = new Knight();
 
-	Knight k1;
-	k1._pet = new Pet();
+		Knight* k3;
 
-	Knight k2;
-	//k2 = static_cast<Knight&&>(k1);
-	k2 = std::move(k1); // << rValue_cast
+		k3 = k1;
+	}
 
-	//TestKnight_LValueRef(k1);
-	//TestKnight_LValueRef(Knight()); << 안됨 (const를 붙여야 함)
-	//TestKnight_ConstLValueRef(Knight());
-	//TestKnight_RValueRef(Knight());
-	//TestKnight_RValueRef(static_cast<Knight&&>(k1));
+	{
+		// 이 방식으로는 refCount가 증가하기 때문에 몇 명이 가리키는지 명확함.
+		// 얕은 복사처럼 동일 포인터를 관리하지만,
+		// 실질적으로는 refCount가 증가하기 때문에 만들어 준 것.
+		SharedPtr<Knight> k1(new Knight());
+		SharedPtr<Knight> k3;
+		k3 = k1;
+	}
+
+	shared_ptr<Knight> k1(new Knight());
+	shared_ptr<Knight> k2(new Knight());
+
+	// 이 순간 refCount가 1 증가함
+	k1->_target = k2;
+
+	// 우리가 원하는 시점에서 소멸시킬 수 없음. (아무도 기억하지 않을 때 삭제되기 때문)
+
+	// 아래의 상황일 시 Attack 함수 내부에서
+	// 딜리트된 메모리를 접근하는 use after free
+	//k1->_target = k2;
+
+	//delete k2;
+
+	//k1->Attack();
 }
