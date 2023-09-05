82898f3a59a7d7741f7b42246f1f68ab588e0060
Kimwoojong <wooj950713@naver.com>
2023-09-05 19:51:33 +0900

    2023/09/05 19:51
    Study
    MultiByte & UniCode

--- GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding.cpp
@@ -11,37 +11,41 @@ using namespace std;
 
 int main()
 {
-	std::string str = "DevWooj";
-	std::string str2 = "DevWooj";
+	// 1) ASCII 코드 'a' = 97, 'A' = 65
+	char ch = 'a';
+	// 7bit (0 ~ 127)
 
-	// 1) 비교 -> ID/PW 맞는지
-	if (str == str2)
-	{
-	}
+	// 2) ANSI >> 동일한 키코드 값 = 동일한 문자
+	// // ASCII + 각 국 언어별로 128
+	// CP949(한국어)
 
-	// 2) 복사
-	string str3;
-	str3 = str;
+	// 3) 유니코드 (동일 번호 = 동일한 문자 = 동일한 유니코드)
+	// 인코딩(UTF-8)
+	// - UTF-8	: 영어(1), 한국어/중국어(3)
+	// - UTF-16 : 영어(2), 한국어(2), 중국어(2)
 
-	// 3) 추가 ()
-	//str.append("1231231232");
-	str += "1234";
+	// 4) MBCS(Multi Bytes Character Set) vs WBCS(Wide Byte Character Set)
+	// - 멀티바이트 집합	: 가변 길이 인코딩	>> UTF-8
+	// - 유니코드 집합	: 고정 길이 인코딩	>> UTF-16
 
-	// 4) 찾기
-	auto c = str.find("Dev");
-	if (c == std::string::npos)
-	{
-		cout << "Not Found" << endl;
-	}
+	setlocale(LC_ALL, "");
+	cout << "LC_ALL : " << setlocale(LC_ALL, NULL) << endl;
 
-	// 5) 교체
-	string chatStr = "SHIT !!!";
-	string findStr = "SHIT";
-	string replaceStr = "****";
+	// CP949(한국어)
+	const char* test = "안녕하세요";
+	cout << test << endl;
 
-	chatStr.replace(chatStr.find(findStr), findStr.length(), replaceStr);
+	//
+	auto test2 = u8"aaa김우종입니당";
+	setlocale(LC_ALL, "en_US.UTF-8");
+	cout << test2 << endl;
 
-	string str4 = str.substr(0, 3);
+	//
+	auto test3 = u"김우종데스";
 
-	const char* name = str.c_str();
+	// char // string
+	wchar_t ch1 = L'김';
+	wstring name = L"김우종데스웅";
+
+	wcout << name << endl;
 }
